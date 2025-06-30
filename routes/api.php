c2459d106864aa7751dfb215e20df036c6883295
Johan Alexander Farfan Sierra <johanfarfan25@gmail.com>
2025-06-30 12:13:05 -0500

    cors

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
+use App\Http\Controllers\RoomController;
 
-Route::controller(HotelController::class)->group(function () {
-    Route::get('hotels', 'index');
-    Route::post('hotels', 'store');
-    Route::put('hotels/{uuid}', 'update');
-    Route::get('hotels/one/{uuid}', 'show');
-    Route::delete('hotels/{uuid}', 'destroy');
-});
+// Aplica CORS a todas las rutas API
+Route::middleware(['cors'])->group(function () {
+    
+    Route::controller(HotelController::class)->group(function () {
+        Route::get('hotels', 'index');
+        Route::post('hotels', 'store');
+        Route::put('hotels/{uuid}', 'update');
+        Route::get('hotels/one/{uuid}', 'show');
+        Route::delete('hotels/{uuid}', 'destroy');
+    });
 
-Route::controller(\App\Http\Controllers\RoomController::class)->group(function () {
-    Route::get('rooms', 'index');
-    Route::post('rooms', 'store');
-    Route::put('rooms/{uuid}', 'update');
-    Route::get('rooms/one/{uuid}', 'show');
-    Route::delete('rooms/{uuid}', 'destroy');
-    Route::get('rooms/hotel/{hotelUuid}', 'shwowByHotel');
+    Route::controller(RoomController::class)->group(function () {
+        Route::get('rooms', 'index');
+        Route::post('rooms', 'store');
+        Route::put('rooms/{uuid}', 'update');
+        Route::get('rooms/one/{uuid}', 'show');
+        Route::delete('rooms/{uuid}', 'destroy');
+        Route::get('rooms/hotel/{hotelUuid}', 'showByHotel');
+    });
+    
 });
