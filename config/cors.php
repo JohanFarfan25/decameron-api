f347c2c3202cab3168e11ad273825d96e39b61ec
Johan Alexander Farfan Sierra <johanfarfan25@gmail.com>
2025-06-30 12:33:18 -0500

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
     'allowed_origins' => ['https://front-decameron.vercel.app'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 86400, // ← Cache de 24 horas
+    'max_age' => 0,
     'supports_credentials' => false,
 ];
